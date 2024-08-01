9a085e47946ae7b281d18d22fe13c5de1b03d1f0
Oswald Sachio <oswald.sachio@gmail.com>
2024-08-01 14:07:32 +0700

    01/08/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import Quiz from './Components/Quiz/Quiz'
 import Login from './Components/Login/Login'
 
 const App = () => {
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,29 +2,52 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './Login.css';
 import { FaUser, FaLock } from "react-icons/fa";
+import Quiz from '../Quiz/Quiz';
 
 const Login = () => {
-  return (
-    <div className='wrapper'>
-        <form action="">
-            <h1>Login</h1>
-            <div className='input-box'>
-                <input type="text" name="username" id="username" placeholder="Username" required/>
-                <FaUser className='icon'/>
-            </div>
-            <div className='input-box'>
-                <input type="password" name="password" id="password" placeholder="Password" required/>
-                <FaLock className='icon'/>
-            </div>
-            <button type='submit'>Login</button>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-            <div className='register-link'>
-                <p><a href="#">Register</a></p>
-            </div>
-        </form>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Validation logic
+        if (username === 'admin' && password === 'admin') {
+            setIsLoggedIn(true);
+        // Redirect to another page or perform further actions
+        } else {
+        setError('Invalid username or password');
+        }
+    };
 
-    </div>
-  )
+    if (isLoggedIn) {
+        return <Quiz />; // Render Quiz component if logged in
+    }
+
+    return (
+        <div className='wrapper'>
+            <form onSubmit={handleSubmit}>
+                <h1>Login</h1>
+                <div className='input-box'>
+                    <input type="text" name="username" id="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}
+            required/>
+                    <FaUser className='icon'/>
+                </div>
+                <div className='input-box'>
+                    <input type="password" name="password" id="password" placeholder="Password"  value={password} onChange={(e) => setPassword(e.target.value)}
+                required/>
+                    <FaLock className='icon'/>
+                </div>
+                <button type='submit'>Login</button>
+
+                <div className='register-link'>
+                    <p><a href="#">Register</a></p>
+                </div>
+            </form>
+
+        </div>
+    )
 }
 
 export default Login
